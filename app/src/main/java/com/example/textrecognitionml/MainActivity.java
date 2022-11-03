df9c46437db5546c3c7acb93db000d3361ec3176
haseebmalik22 <haseebmalik.info@gmail.com>
2022-11-03 01:39:54 -0700

    1st commit

---
+++ app/src/main/java/com/example/textrecognitionml/MainActivity.java
@@ -0,0 +1,142 @@
+package com.example.textrecognitionml;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    TextView resultq;
+    Uri filepath;
+    Bitmap bitmap;
+    Button choose;
+    ActivityResultLauncher<Intent> activityResultLauncher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imgpick);
+
+        resultq = findViewById(R.id.txtpick);
+        choose = findViewById(R.id.btnpick);
+
+
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode()==RESULT_OK && result.getData() != null){
+                    filepath=result.getData().getData();
+
+                    try {
+                        InputStream inputStream = getContentResolver().openInputStream(filepath);
+                        bitmap = BitmapFactory.decodeStream(inputStream);
+                        imageView.setImageBitmap(bitmap);
+
+                        FirebaseVisionImage image;
+                        try {
+                            image = FirebaseVisionImage.fromFilePath(getApplicationContext(),filepath);
+                            FirebaseVisionTextRecognizer textRecognizer = FirebaseVision.getInstance()
+                                    .getCloudTextRecognizer();
+
+                            textRecognizer.processImage(image)
+                                    .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                                        @Override
+                                        public void onSuccess(FirebaseVisionText result) {
+
+                                            resultq.setText(result.getText());
+                                        }
+                                    })
+                                    .addOnFailureListener(
+                                            new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    Toast.makeText(MainActivity.this, "failed", Toast.LENGTH_SHORT).show();
+                                                    // Task failed with an exception
+                                                    // ...
+                                                }
+                                            });
+
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                    }catch (Exception ex){
+
+                    }
+                }
+
+            }
+        });
+
+
+        choose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dexter.withActivity(MainActivity.this)
+                        .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                        .withListener(new PermissionListener() {
+                            @Override
+                            public void onPermissionGranted(PermissionGrantedResponse response) {
+                                Intent intent = new Intent(Intent.ACTION_PICK);
+                                intent.setType("image/*");
+                                activityResultLauncher.launch(intent);
+
+                            }
+
+                            @Override
+                            public void onPermissionDenied(PermissionDeniedResponse response) {
+
+                            }
+
+                            @Override
+                            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                                token.continuePermissionRequest();
+                            }
+                        }).check();
+
+
+            }
+        });
+
+
+
+
+
+    }
+}
